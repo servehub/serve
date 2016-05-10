c8db72a58553260d946f8579f541f36b3e46a341
kulikov <kulikov.dm@gmail.com>
2016-05-10 10:04:48 +0300

    + supervisor

---
+++ supervisor/cmd.go
@@ -0,0 +1,37 @@
+package supervisor
+
+import (
+	"github.com/codegangsta/cli"
+	"io/ioutil"
+	"log"
+)
+
+func SupervisorCommand() cli.Command {
+	return cli.Command{
+		Name:  "supervisor",
+		Flags: []cli.Flag{
+			cli.IntFlag{Name: "max-retry", Value: -1},
+		},
+		Subcommands: []cli.Command{
+			{
+				Name:            "start",
+				SkipFlagParsing: true,
+				Action: func(c *cli.Context) {
+					log.Println("Starting", c.Args())
+
+					super := Supervisor{
+						MaxRetry: c.Int("max-retry"),
+						OnError: func() {
+							log.Println("Error")
+						},
+						OnComplete: func() {
+							log.Println("Complete")
+						},
+					}
+
+					super.Run(c.Args())
+				},
+			},
+		},
+	}
+}
