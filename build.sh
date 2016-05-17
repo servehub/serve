daee4f417d763c22c462a5d773a783c7812dbbb6
kulikov <kulikov.dm@gmail.com>
2016-05-17 13:24:07 +0300

    = fix build (install golang to /var/go/.golang)

--- build.sh
+++ build.sh
@@ -3,22 +3,23 @@
 ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
 # install golang
-if [ ! -x /usr/local/go/bin/go ]; then
+if [ ! -x /var/go/.golang/go/bin/go ]; then
   wget https://storage.googleapis.com/golang/go1.6.2.linux-amd64.tar.gz -O /tmp/go1.6.2.linux-amd64.tar.gz
-  sudo tar -C /usr/local -xzvf /tmp/go1.6.2.linux-amd64.tar.gz
-  sudo ln -vfns /usr/local/go/bin/go /usr/local/bin/go
+  mkdir -p /var/go/.golang
+  tar -C /var/go/.golang -xzvf /tmp/go1.6.2.linux-amd64.tar.gz
   rm -f /tmp/go1.6.2.linux-amd64.tar.gz
 fi
 
 # install glide (go deps manager)
-if [ ! -x /usr/local/bin/glide ]; then
+if [ ! -x /var/go/.golang/go/bin/glide ]; then
   wget https://github.com/Masterminds/glide/releases/download/0.10.2/glide-0.10.2-linux-amd64.tar.gz -O /tmp/glide-0.10.2-linux-amd64.tar.gz
   tar -C /tmp/ -xzvf /tmp/glide-0.10.2-linux-amd64.tar.gz
-  sudo cp /tmp/linux-amd64/glide /usr/local/bin/glide
+  cp /tmp/linux-amd64/glide /var/go/.golang/go/bin/glide
   rm -f /tmp/glide-0.10.2-linux-amd64.tar.gz
   rm -rf /tmp/linux-amd64/
 fi
 
+export PATH=$PATH:/var/go/.golang/go/bin
 export GOPATH=$ROOT/.gopath
 
 WORK_DIR=$GOPATH/src/github.com/InnovaCo/serve
