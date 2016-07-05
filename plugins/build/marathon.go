ccb309b3b82254f47ce5bba8375876de4bfc0725
kulikov <kulikov.dm@gmail.com>
2016-07-05 20:49:25 +0300

    + build.marathon

--- plugins/build/marathon.go
+++ plugins/build/marathon.go
@@ -1,32 +1,24 @@
 package build
 
-//import (
-//	"fmt"
-//
-//	"github.com/InnovaCo/serve/manifest"
-//	"github.com/InnovaCo/serve/utils"
-//)
-//
-//type MarathonBuild struct{}
-//
-//func (_ MarathonBuild) Run(m *manifest.Manifest, sub *manifest.Manifest) error {
-//	if err := utils.RunCmdf("tar -zcf package.tar.gz -C %s/ .", sub.GetString("marathon.package")); err != nil {
-//		return err
-//	}
-//
-//	if err := utils.RunCmdf("curl -vsSf -XPUT -T package.tar.gz %s", TaskRegistryUrl(m)); err != nil {
-//		return err
-//	}
-//
-//	return nil
-//}
-//
-//func TaskRegistryUrl(m *manifest.Manifest) string {
-//	return fmt.Sprintf(
-//		"http://%s/task-registry/%s/%s-v%s.tar.gz",
-//		m.GetString("marathon.marathon-host"),
-//		m.ServiceFullName("/"),
-//		m.ServiceName(),
-//		manifest.Escape(m.BuildVersion()),
-//	)
-//}
+import (
+	"github.com/InnovaCo/serve/manifest"
+	"github.com/InnovaCo/serve/utils"
+)
+
+func init() {
+	manifest.PluginRegestry.Add("build.marathon", MarathonBuild{})
+}
+
+type MarathonBuild struct{}
+
+func (p MarathonBuild) Run(data manifest.Manifest) error {
+	if err := utils.RunCmdf("tar -zcf marathon.tar.gz -C %s/ .", data.GetString("source")); err != nil {
+		return err
+	}
+
+	if err := utils.RunCmdf("curl -vsSf -XPUT -T marathon.tar.gz %s", data.GetString("registry_url")); err != nil {
+		return err
+	}
+
+	return nil
+}
