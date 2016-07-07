af2fdb624ee5bf36991c6417929d94b074bd1af0
mhanygin <hanygin_ma@mail.ru>
2016-07-07 14:57:55 +0300

    fix plugins, manifest

--- plugins/gocd.change.go
+++ plugins/gocd.change.go
@@ -10,7 +10,6 @@ import (
 
 func init() {
 	manifest.PluginRegestry.Add("gocd.change", GoCDChange{})
-	manifest.PluginRegestry.Add("gocd.delete", GoCDDelete{})
 }
 
 type GoCDChange struct{}
@@ -73,49 +72,6 @@ func (p GoCDChange) Run(data manifest.Manifest) error {
 	} else {
 		if resp.StatusCode != http.StatusOK {
 			return errors.New("Operation error: " + resp.Status)
-		}
-		return nil
-	}
-}
-
-type GoCDDelete struct{}
-
-/*
-plugin for manifest section "gocd.delete"
-section structure:
-
-gocd.delete:
-	login: LOGIN
-	password: PASSWORD
-	url: GOCD_URL
-	data:
-		group: GROUP
-		pipeline:
-			name: NAME
-
- */
-func (p GoCDDelete) Run(data manifest.Manifest) error {
-	fmt.Println("--> ", data)
-	var name, url string
-
-	login := data.GetString("login")
-	password := data.GetString("password")
-
-	if url = data.GetString("url"); url == "" {
-		return errors.New("GoCD url ot found")
-	}
-
-	if name = data.GetString("data.pipeline.name"); name == "" {
-		return errors.New("GoCD pipeline name not found")
-	}
-
-	if resp, err := request("DELETE", url + "/" + name, "", map[string]string{}, login, password); err != nil {
-		return err
-	} else {
-		if resp.StatusCode == http.StatusOK {
-			return nil
-		} else {
-			errors.New("delete pipeline error: " + resp.Status)
 		}
 		return nil
 	}
