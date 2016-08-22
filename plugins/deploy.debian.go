92f6db0cea36c53413df0a63f476757df282fc28
mhanygin <hanygin_ma@mail.ru>
2016-08-22 15:10:15 +0300

    fix

--- plugins/deploy.debian.go
+++ plugins/deploy.debian.go
@@ -53,3 +53,12 @@ func runSshCmd(cluster, sshUser, cmd string) error {
 		cmd,
 	)
 }
+
+func runSshCmdSingle(cluster, sshUser, cmd string) error {
+	return utils.RunCmd(
+		`ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no %s@{} "%s"`,
+		cluster,
+		sshUser,
+		cmd,
+	)
+}
