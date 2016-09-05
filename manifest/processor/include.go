f2cf78bc9de688593c54fdf5552e2f2a19f322e2
mhanygin <hanygin_ma@mail.ru>
2016-09-05 11:59:31 +0300

    fix

--- manifest/processor/include.go
+++ manifest/processor/include.go
@@ -15,6 +15,11 @@ const ConfigPath = "/etc/serve"
 type Include struct{}
 
 func (in Include) Process(tree *gabs.Container) error {
+	path := ConfigPath
+	if path, ok := tree.Search("vars", "config-path").Data().(string); ok {
+		path = path
+	}
+
 	if tree.ExistsP("include") {
 		items, err := tree.Path("include").Children()
 		if err != nil {
@@ -24,7 +29,7 @@ func (in Include) Process(tree *gabs.Container) error {
 		for _, inc := range items {
 			if file, ok := inc.Search("file").Data().(string); ok {
 				if !strings.HasPrefix(file, "/") {
-					file = ConfigPath + "/" + file
+					file = path + "/" + file
 				}
 
 				if err := includeFile(file, tree); err != nil {
@@ -35,7 +40,7 @@ func (in Include) Process(tree *gabs.Container) error {
 	}
 
 	// include all base configs
-	if files, err := filepath.Glob(ConfigPath + "/conf.d/*.yml"); err == nil {
+	if files, err := filepath.Glob(path + "/conf.d/*.yml"); err == nil {
 		for _, file := range files {
 			if err := includeFile(file, tree); err != nil {
 				return err
