715aceb17345e22fbac633ed36ac29977127606b
kulikov <kulikov.dm@gmail.com>
2016-11-14 20:33:46 +0300

    = test: change workdir name to tests

--- plugins/test/autotest.go
+++ plugins/test/autotest.go
@@ -14,12 +14,12 @@ func init() {
 type TestAutotest struct{}
 
 func (p TestAutotest) Run(data manifest.Manifest) error {
-	if err := utils.RunCmd("rm -rf autotests && git clone --depth 1 --single-branch --recursive %s autotests", data.GetString("repo")); err != nil {
+	if err := utils.RunCmd("rm -rf tests && git clone --depth 1 --single-branch --recursive %s tests", data.GetString("repo")); err != nil {
 		return fmt.Errorf("Error on clone test git repo: %v", err)
 	}
 
 	return utils.RunCmd(
-		"cd autotests/ && ./test.sh --project=%s --version=%s --suite=%s",
+		"cd tests/ && ./test.sh --project=%s --version=%s --suite=%s",
 		data.GetString("project"),
 		data.GetString("version"),
 		data.GetString("suite"),
