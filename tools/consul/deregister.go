64ef31f1cbf58d7b5e89a1fa163a5162d5cb1099
kulikov <kulikov.dm@gmail.com>
2016-11-17 20:27:15 +0300

    = deregistering by serv name

--- tools/consul/deregister.go
+++ tools/consul/deregister.go
@@ -2,6 +2,8 @@ package consul
 
 import (
 	"log"
+	"strings"
+	"encoding/json"
 
 	"github.com/codegangsta/cli"
 	"github.com/hashicorp/consul/api"
@@ -14,26 +16,60 @@ func DeregisterCommand() cli.Command {
 		Flags: []cli.Flag{
 			cli.BoolFlag{Name: "dry-run"},
 		},
-		Action: func(c *cli.Context) error {
-			consul, _ := api.NewClient(api.DefaultConfig())
+		Subcommands: []cli.Command{
+			{
+				Name:  "service",
+				Action: func(c *cli.Context) error {
+					consul, _ := api.NewClient(api.DefaultConfig())
 
-			checks, err := consul.Agent().Checks()
-			if err != nil {
-				return err
-			}
+					name := c.Args().First()
+					if name == "" {
+						return f
+					}
 
-			for _, check := range checks {
-				if check.Status == "critical" {
-					log.Println("Deregistering", check.ServiceID, "=", check.Status)
-
-					err := consul.Agent().ServiceDeregister(check.ServiceID)
+					servs, err := consul.Agent().Services()
 					if err != nil {
 						return err
 					}
-				}
-			}
 
-			return nil
+					js, _ := json.MarshalIndent(servs, "", "  ")
+					println(string(js))
+
+					return nil
+				},
+			},
+			{
+				Name:  "unhealth",
+				Usage: "Deregister all unhealth services on cluster",
+				Action: func(c *cli.Context) error {
+					consul, _ := api.NewClient(api.DefaultConfig())
+
+					checks, err := consul.Agent().Checks()
+					if err != nil {
+						return err
+					}
+
+					js, _ := json.MarshalIndent(checks, "", "  ")
+					println(string(js))
+
+					name := c.String("service")
+
+					for _, check := range checks {
+						if check.Status == "critical" || (name != "" && strings.Contains(check.ServiceID, name)) {
+							log.Println("Deregistering", check.ServiceID, "=", check.Status)
+
+							if !c.GlobalBool("dry-run") {
+								err := consul.Agent().ServiceDeregister(check.ServiceID)
+								if err != nil {
+									return err
+								}
+							}
+						}
+					}
+
+					return nil
+				},
+			},
 		},
 	}
 }
