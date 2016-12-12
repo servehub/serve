705a3ae0a54c58565643a896a0d80b8885f7b07f
mhanygin <hanygin_ma@mail.ru>
2016-12-12 15:43:57 +0300

    add plugin build test

---
+++ plugins/build/sbt_test.go
@@ -0,0 +1,19 @@
+package build
+
+import (
+	"testing"
+)
+
+func TestSbtBuild(t *testing.T) {
+	runAllMultiCmdTests(t, map[string]processorTestCase{
+		"simple": {
+			in: `---
+version: "0.0.0"
+test: "testOnly -- -l Integration"`,
+			expect: map[string]interface{}{
+				"cmdline": []string{"sbt ';set every version := \"0.0.0\"' clean \"testOnly -- -l Integration\" "},
+				},
+		},
+	},
+	SbtBuild{})
+}
