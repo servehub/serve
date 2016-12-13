e08129797a78f2b3a62407fbdec6dbf47dde215b
mhanygin <hanygin_ma@mail.ru>
2016-12-13 18:39:06 +0300

    add plugin deploy/release test

---
+++ plugins/deploy/tarball_test.go
@@ -0,0 +1,59 @@
+package deploy
+
+import (
+	"testing"
+)
+
+func TestDeployTarball(t *testing.T) {
+	runAllMultiCmdTests(t, map[string]processorTestCase{
+		"install": {
+			in: `---
+cluster: "test.ru"
+ssh-user: "test_user"
+package-name: "test_name"
+package-uri: "test_name.ru"
+install-root: "/local/test/tarball"
+user: "user"
+group: "group"
+consul-address: "consul.test.ru"
+hooks: []`,
+			expect: map[string]interface{}{
+				"cmdline": []string{"dig +short test.ru | sort | uniq | parallel --tag --line-buffer -j 50 ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null test_user@{} \"curl -vsSf -o /tmp/tarball-RANDOM_NAME.tar.gz test_name.ru && rm -rf /tmp/tarball-RANDOM_NAME/ && mkdir -p /tmp/tarball-RANDOM_NAME/ && tar xzf /tmp/tarball-RANDOM_NAME.tar.gz -C /tmp/tarball-RANDOM_NAME/ && sudo rm -rf /local/test/tarball/test_name && sudo mkdir -p /local/test/tarball/test_name && sudo mv /tmp/tarball-RANDOM_NAME/* /local/test/tarball/test_name/ && sudo chown -R user:group /local/test/tarball/test_name/ && rm -rf /tmp/tarball-RANDOM_NAME.tar.gz /tmp/tarball-RANDOM_NAME/\""},
+				},
+		},
+		"install with hooks": {
+			in: `---
+cluster: "test.ru"
+ssh-user: "test_user"
+package-name: "test_name"
+package-uri: "test_name.ru"
+install-root: "/local/test/tarball"
+user: "user"
+group: "group"
+consul-address: "consul.test.ru"
+hooks:
+  - postinstall: test1.sh
+  - postinstall: test2.sh`,
+			expect: map[string]interface{}{
+				"cmdline": []string{"dig +short test.ru | sort | uniq | parallel --tag --line-buffer -j 50 ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null test_user@{} \"curl -vsSf -o /tmp/tarball-RANDOM_NAME.tar.gz test_name.ru && rm -rf /tmp/tarball-RANDOM_NAME/ && mkdir -p /tmp/tarball-RANDOM_NAME/ && tar xzf /tmp/tarball-RANDOM_NAME.tar.gz -C /tmp/tarball-RANDOM_NAME/ && sudo rm -rf /local/test/tarball/test_name && sudo mkdir -p /local/test/tarball/test_name && sudo mv /tmp/tarball-RANDOM_NAME/* /local/test/tarball/test_name/ && sudo chown -R user:group /local/test/tarball/test_name/ && rm -rf /tmp/tarball-RANDOM_NAME.tar.gz /tmp/tarball-RANDOM_NAME/ && sudo test1.sh && sudo test2.sh\""},
+				},
+		},
+		"uninstall": {
+			in: `---
+cluster: "test.ru"
+ssh-user: "test_user"
+package-name: "test_name"
+package-uri: "test_name.ru"
+install-root: "/local/test/tarball"
+user: "user"
+group: "group"
+consul-address: "consul.test.ru"
+hooks: []
+purge: true`,
+			expect: map[string]interface{}{
+				"cmdline": []string{"dig +short test.ru | sort | uniq | parallel --tag --line-buffer -j 50 ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null test_user@{} \"sudo rm -rf /local/test/tarball/test_name\""},
+				},
+		},
+	},
+	DeployTarball{})
+}
---
+++ plugins/release/debian_test.go
@@ -0,0 +1,65 @@
+package release
+
+import (
+	"fmt"
+	"testing"
+	"github.com/fatih/color"
+
+	"github.com/ghodss/yaml"
+
+	"github.com/InnovaCo/serve/manifest"
+	"github.com/InnovaCo/serve/utils"
+)
+
+func loadTestData(data string, plugin manifest.Plugin) error {
+	if json, err := yaml.YAMLToJSON([]byte(data)); err != nil {
+		return fmt.Errorf("Parser error: %v", err)
+	} else {
+		return plugin.Run(*manifest.LoadJSON(string(json)))
+	}
+}
+
+type processorTestCase struct {
+	in     string
+	expect map[string]interface{}
+}
+
+func TestReleaseDebian(t *testing.T) {
+	runAllMultiCmdTests(t, map[string]processorTestCase{
+		"simple": {
+			in: `---
+cluster: "test.ru"
+ssh-user: test_user
+ci-tools-path: /var/test
+package: package
+site: test-site
+mode: stage`,
+			expect: map[string]interface{}{
+				"cmdline": []string{"dig +short test.ru | sort | uniq | parallel --tag --line-buffer -j 1 ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null test_user@{} \"sudo /var/test/debian-way/release.sh --package='package' --site='test-site' --mode='stage'\""},
+				},
+		},
+	},
+	ReleaseDebian{})
+}
+
+func runAllMultiCmdTests(t *testing.T, cases map[string]processorTestCase, plugin manifest.Plugin) {
+	color.NoColor = false
+
+	for name, test := range cases {
+		utils.RunCmdWithEnv = func(cmdline string, env map[string]string) error {
+			for _, v := range test.expect["cmdline"].([]string) {
+				if v == cmdline {
+					return nil
+				}
+			}
+			return fmt.Errorf("cmd: %v not found in %v", cmdline, test.expect["cmdline"].([]string))
+		}
+
+		if err := loadTestData(test.in, plugin); err == nil {
+			color.Green("%v: Ok\n", name)
+		} else {
+			color.Red("error %v\n: failed!", err)
+			t.Fail()
+		}
+	}
+}
