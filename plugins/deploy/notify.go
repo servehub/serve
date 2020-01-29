2e1f0a0e884dcea60204d9d6dd7b3e0b36404c5f
kulikov <kulikov.dm@gmail.com>
2020-01-29 16:08:59 +0300

    add build.notify and deploy.notify plugins

---
+++ plugins/deploy/notify.go
@@ -0,0 +1,32 @@
+package deploy
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/servehub/serve/manifest"
+	"github.com/servehub/utils"
+)
+
+func init() {
+	manifest.PluginRegestry.Add("deploy.notify", DeployNotify{})
+}
+
+type DeployNotify struct{}
+
+func (p DeployNotify) Run(data manifest.Manifest) error {
+	manifestArg := ""
+	if data.Has("manifest") {
+		manifestArg = fmt.Sprintf(`--manifest="%s"`, data.GetString("manifest"))
+	}
+
+	return utils.RunCmd(
+		`serve notify --env=%s --event="%s" --message="%s" --build-number="%s" --changelog-for="%s" %s`,
+		data.GetString("env"),
+		data.GetString("event"),
+		strings.Replace(data.GetString("message"), `"`, `”`, -1),
+		data.GetString("build-number"),
+		data.GetString("changelog-for"),
+		manifestArg,
+	)
+}
