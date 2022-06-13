92718bb650c11cda0e49a1b30823ac56c11e13a3
Dmitry Kulikov <kulikov.dm@gmail.com>
2022-06-13 15:08:35 +0100

    fix test.coverage config

--- plugins/test/coverage.go
+++ plugins/test/coverage.go
@@ -71,7 +71,7 @@ func (p CoverageUpload) Run(data manifest.Manifest) error {
 		}).Error; err != nil {
 			return fmt.Errorf("failed to upload coverage exec file: %w", err)
 		}
-		
+
 		return nil
 	}
 
@@ -107,15 +107,15 @@ func (p CoverageUpload) Run(data manifest.Manifest) error {
 
 	if diff < 0 {
 		return github.SendStatus(accessToken, meta.Repo, meta.Ref, "success",
-			fmt.Sprintf("Thank you for increasing the test coverage by %.2f%", -diff),
+			fmt.Sprintf("Thank you for increasing the test coverage by %.2f%%", -diff),
 			statusContext, targetUrl)
 	} else if diff < coverageTolerance {
 		return github.SendStatus(accessToken, meta.Repo, meta.Ref, "success",
-			fmt.Sprintf("Coverage changed by %.2f%", diff),
+			fmt.Sprintf("Coverage changed by %.2f%%", diff),
 			statusContext, targetUrl)
 	} else {
 		return github.SendStatus(accessToken, meta.Repo, meta.Ref, "failure",
-			fmt.Sprintf("Please increase test coverage at least by %.2f%", diff),
+			fmt.Sprintf("Please increase test coverage at least by %.2f%%", diff),
 			statusContext, targetUrl)
 	}
 }
