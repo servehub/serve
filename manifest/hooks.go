8b1171cb4de3f4a285e97b7c9a0ca037175921c7
Dmitry Kulikov <kulikov.dm@gmail.com>
2022-07-14 11:25:23 +0100

    hooks: keep ordering

--- manifest/hooks.go
+++ manifest/hooks.go
@@ -1,6 +1,7 @@
 package manifest
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/fatih/color"
@@ -13,18 +14,20 @@ type Hooks struct {
 }
 
 func (h *Hooks) Run(hookName string) error {
-	if commands, err := h.Manifest.tree.Search("hooks", hookName).ChildrenMap(); err == nil {
-		for k, cmd := range commands {
-			log.Println(color.MagentaString("> %s / %s:", hookName, k))
+	if commands, err := h.Manifest.tree.Search("hooks", hookName).Children(); err == nil {
+		for _, cmd := range commands {
+			log.Println(color.MagentaString("> %s:", hookName))
 
 			if !h.DryRun {
 				if err := utils.RunCmd(`%s`, cmd.Data()); err != nil {
-					log.Println(color.RedString("Error on run %s / %s: %v", hookName, k, err))
+					log.Println(color.RedString("Error on run %s: %v", hookName, err))
 					return err
 				}
 			} else {
 				log.Printf("%s", cmd.Data())
 			}
+
+			fmt.Println("")
 		}
 	}
 
